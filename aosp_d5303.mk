f950b7a6eb9bb298729aa572c17533a7d3db3266
Adam Farden <adam@farden.cz>
2014-12-28 10:41:55 +0100

    cm: move from sony/yukon to sony/msm8226-common
    Change-Id: Idaeaedc92cba73fe41f7ddf37c8c00b92bd6f091

--- aosp_d5303.mk
+++ aosp_d5303.mk
@@ -18,7 +18,7 @@ BOARD_HAS_DIM_BACKLIGHT := true
 
 $(call inherit-product, $(SRC_TARGET_DIR)/product/aosp_base.mk)
 $(call inherit-product, $(SRC_TARGET_DIR)/product/telephony.mk)
-$(call inherit-product, device/sony/yukon/device.mk)
+$(call inherit-product, device/sony/msm8226-common/device.mk)
 $(call inherit-product, vendor/sony/tianchi/tianchi-vendor.mk)
 $(call inherit-product-if-exists, frameworks/native/build/phone-xhdpi-1024-dalvik-heap.mk)
 
